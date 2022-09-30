6b08776cd0e89d9e182148a259025ee01b562a0a
IsmoiljonJabborov <190107106@stu.sdu.edu.kz>
2022-09-30 12:05:13 +0500

    These are somehow understandable!

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public static void roundNumber(float numberToBeRounded) {
+        int round = (int) numberToBeRounded;
+        System.out.println(round);
     }
 }
